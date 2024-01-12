83d150c0103c1031bd0ef60c10f0f9116ffa7c89
Hachem Mohamed <hechemmohamed9@gmail.com>
2024-01-12 14:24:59 +0100

    first commit

---
+++ sr-traffic-main/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TrafficMessagesComponent } from './components/traffic-info/traffic-messages.component';
+
+const routes: Routes = [
+    { path: '**', component: TrafficMessagesComponent }
+    
+   
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, {})],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
